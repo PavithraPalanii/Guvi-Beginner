9c9a6d77e69293b5eac7a61ffd05e4f3436d4036
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-12 09:37:11 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,31 @@
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+		
+int b=5;
+
+if(b>0)
+				
+{
+System.out.println("positive");
+				
+}
+				
+else if(b<0)
+				
+{
+					
+System.out.println("negative");
+				
+}
+				
+else
+				
+{
+					
+System.out.println("zero");
+				
+}
+
+}
+
