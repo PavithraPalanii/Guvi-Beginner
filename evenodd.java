03fd94b29b4ad6c151a6d0d6f4b9a3cc62e59135
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-13 09:17:03 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,19 @@
+class evenodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int b=3;
+		if(b%2==0)
+		{
+			System.out.print("even");
+		}
+		else if(b%2!=0)
+		{
+			System.out.print("odd");
+		}
+		else
+		{
+			System.out.print("invalid");
+		}
+	}
+}
